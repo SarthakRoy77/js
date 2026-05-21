9df942ef301b5309ca0fd9ca29bcb536bbe4fc55
Sarthak Roy <p-2066-19@dpspatna.com>
2026-05-21 10:22:39 +0530

    Final commit to Project : added Budget system and Age system

--- ZooOrganizer/zoo.js
+++ ZooOrganizer/zoo.js
@@ -1,6 +1,13 @@
 // In this project we will create an Organizer of Animals and staff for a zoo and also add some methods to them . The topic in large covered is
 // Object-Oriented - Programming also known as OOP
 
+//Let us create an Assign Budget function
+
+function assignBudget(budget) {
+    ZooStaff.ZooBudget = budget;
+    console.log(`Assigned Zoo Budget to ${budget}`)
+}
+
 // Let us create a Parent Animal class
 class ZooAnimal {
 
@@ -15,6 +22,7 @@ class ZooAnimal {
         ZooAnimal.animalsRecorded++ ;
 
     }
+
     static revealAnimalFrequency() {
         console.log(`${ZooAnimal.animalsRecorded} animals have been recorded`);
     }
@@ -29,22 +37,26 @@ class ZooAnimal {
 
     celebrateBirthday() {
         console.log(`Happy ${this.age}th Birthday to ${this.name} `);
+        this.age = this.age + 1;
+        console.log(`${this.name}'s age now is ${this.age}`);
     }
 }
 
 //Let us create a Parent Staff Class , The classes have been designed to be used as a parent class, although it does not have a child class
 class ZooStaff{
     static staffRecorded = 0;
+    static ZooBudget = 1000000;
 
     constructor(name, age, previousExperience, wage ) {
+        ZooStaff.staffRecorded++;
         this.name = name;
         this.age = age;
         this.previousExperience = previousExperience;
         this.wage = wage;
-
-        ZooStaff.staffRecorded++;
+        this.roll = ZooStaff.staffRecorded;
 
     }
+
 
     sendToWork(){
         console.log(`Sent ${this.name} to work `);
@@ -52,14 +64,22 @@ class ZooStaff{
 
     payStaff() {
         console.log(`Paid ${this.wage} to ${this.name}`);
+        ZooStaff.UpdateBudget(this.wage);
     }
 
     celebrateBirthday() {
         console.log(`Happy ${this.age}th Birthday to ${this.name} `);
+        this.age = this.age + 1;
+        console.log(`${this.name}'s age now is ${this.age}`);
     }
 
     static revealStaffFrequency() {
         console.log(`${ZooStaff.staffRecorded} staff has been hired`);
+    }
+
+    static UpdateBudget(wage) {
+        ZooStaff.ZooBudget = ZooStaff.ZooBudget - wage;
+        console.log(`After paying the updated budget is ${ZooStaff.ZooBudget}`)
     }
 }
 
@@ -89,6 +109,10 @@ console.log(ZooKeeper.previousExperience);
 console.log(ZooKeeper.name);
 console.log(Manager.wage);
 
+assignBudget(50000)
+
 ZooKeeper.payStaff();
 ZooKeeper.celebrateBirthday();
 Manager.sendToWork()
+
+console.log(ZooKeeper.roll);
